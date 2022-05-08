f473f5493ee8401c21111575b475c8e8c3f8efe1
Werner87 <95691156+Werner87@users.noreply.github.com>
2022-05-08 21:37:13 +0200

    Add files via upload

---
+++ src/SearchAlgorithms.Core/Algorithms/KMPAlgoritm.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace SearchAlgorithms.Core.Algorithms
+
+{
+
+    public class KMPAlgoritm : ISearchAlgorithm
+
+    {
+
+        public string Name()
+
+        {
+
+            return "Knutha-Morrisa-Pratta";
+
+        }
+
+        public List<int> Search(in string lookingString, in string longString)
+
+        {
+            void KMPSearch(string lookingString, string longString)
+            {
+                int M = lookingString.Length;
+                int N = longString.Length;
+
+                int[] lps = new int[M];
+                int j = 0;
+
+                computeLPSArray(lookingString, M, lps);
+
+                int i = 0;
+                while (i < N)
+                {
+                    if (lookingString[j] == longString[i])
+                    {
+                        j++;
+                        i++;
+                    }
+                    if (j == M)
+                    {
+                        Console.Write("Wzorzec dopasowamny "
+                                      + "na miejscu " + (i - j));
+                        j = lps[j - 1];
+                    }
+
+                    else if (i < N && lookingString[j] != longString[i])
+                    {
+
+                        if (j != 0)
+                            j = lps[j - 1];
+                        else
+                            i = i + 1;
+                    }
+                }
+            }
+
+            void computeLPSArray(string lookingString, int a, int[] lps)
+            {
+                int length = 0;
+                int i = 1;
+                lps[0] = 0;
+                while (i < a)
+                {
+                    if (lookingString[i] == lookingString[length])
+                    {
+                        length++;
+                        lps[i] = length;
+                        i++;
+                    }
+                    else
+                    {
+                        if (length != 0)
+                            length = lps[length - 1];
+                        else
+                        {
+                            lps[i] = length;
+                        }
+                    }
+
+                }
+
+            }
+
+            int a = lookingString.Length;
+            int b = longString.Length;
+            int[] lps = new int[a]; //lps holds longest prefix sufix
+            int j = 0;
+            computeLPSArray(lookingString, a, lps);
+
+        }
+
+    }
+
+}
