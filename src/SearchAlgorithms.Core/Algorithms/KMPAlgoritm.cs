8c71805abaecde6204f4e349c71cfdd8ba82909a
Daniel Rogowski <daniel.rogowski@onet.pl>
2022-05-08 22:13:14 +0200

    Partially fixed KMP implementation

--- src/SearchAlgorithms.Core/Algorithms/KMPAlgoritm.cs
+++ src/SearchAlgorithms.Core/Algorithms/KMPAlgoritm.cs
@@ -3,95 +3,77 @@ using System.Collections.Generic;
 
 
 namespace SearchAlgorithms.Core.Algorithms
-
 {
-
     public class KMPAlgoritm : ISearchAlgorithm
-
     {
 
         public string Name()
-
         {
-
             return "Knutha-Morrisa-Pratta";
-
         }
 
-        public List<int> Search(in string lookingString, in string longString)
+        public List<int> Search(in string lookingString, in string longString) {
+            var result = new List<int>();
 
-        {
-            void KMPSearch(string lookingString, string longString)
+            int M = lookingString.Length;
+            int N = longString.Length;
+
+            int[] lps = new int[M];
+            int j = 0;
+
+            ComputeLPSArray(lookingString, M, lps);
+
+            int i = 0;
+            while (i < N)
             {
-                int M = lookingString.Length;
-                int N = longString.Length;
-
-                int[] lps = new int[M];
-                int j = 0;
-
-                computeLPSArray(lookingString, M, lps);
-
-                int i = 0;
-                while (i < N)
+                if (lookingString[j] == longString[i])
                 {
-                    if (lookingString[j] == longString[i])
-                    {
-                        j++;
-                        i++;
-                    }
-                    if (j == M)
-                    {
-                        Console.Write("Wzorzec dopasowamny "
-                                      + "na miejscu " + (i - j));
+                    j++;
+                    i++;
+                }
+                if (j == M)
+                {
+                    result.Add(i - j);
+                    j = lps[j - 1];
+                }
+
+                else if (i < N && lookingString[j] != longString[i])
+                {
+
+                    if (j != 0)
                         j = lps[j - 1];
-                    }
-
-                    else if (i < N && lookingString[j] != longString[i])
-                    {
-
-                        if (j != 0)
-                            j = lps[j - 1];
-                        else
-                            i = i + 1;
-                    }
+                    else
+                        i = i + 1;
                 }
             }
 
-            void computeLPSArray(string lookingString, int a, int[] lps)
+            return result;
+        }
+
+        private static void ComputeLPSArray(string lookingString, int a, int[] lps)
+        {
+            int length = 0;
+            int i = 1;
+            lps[0] = 0;
+            while (i < a)
             {
-                int length = 0;
-                int i = 1;
-                lps[0] = 0;
-                while (i < a)
+                if (lookingString[i] == lookingString[length])
                 {
-                    if (lookingString[i] == lookingString[length])
-                    {
-                        length++;
-                        lps[i] = length;
-                        i++;
-                    }
+                    length++;
+                    lps[i] = length;
+                    i++;
+                }
+                else
+                {
+                    if (length != 0)
+                        length = lps[length - 1];
                     else
                     {
-                        if (length != 0)
-                            length = lps[length - 1];
-                        else
-                        {
-                            lps[i] = length;
-                        }
+                        lps[i] = length;
                     }
-
                 }
 
             }
-
-            int a = lookingString.Length;
-            int b = longString.Length;
-            int[] lps = new int[a]; //lps holds longest prefix sufix
-            int j = 0;
-            computeLPSArray(lookingString, a, lps);
-
         }
-
     }
-
 }
