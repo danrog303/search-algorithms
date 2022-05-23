59174e35c35990715244577cbc2dcb5af507aac8
imparpaing <saintsikorski@gmail.com>
2022-05-23 20:47:58 +0200

    Enhance composition of time measurement code

---
+++ src/SearchAlgorithms.Core/Testing/Timers/PrimeNumbersTimeMeasure.cs
@@ -0,0 +1,18 @@
+﻿using SearchAlgorithms.Core.Utils;
+using System;
+using System.Collections.Generic;
+namespace SearchAlgorithms.Core.Testing.Timers
+{
+    public class PrimeNumbersTimeMeasure : UnifiedUnitTimeMeasure
+    {
+        private static double MeasureReferentialUnit(long numbersToCalculate)
+        {
+            var timer = new TimeMeasure(() => PrimeNumberUtils.FindNthPrimeNumber(numbersToCalculate));
+            return timer.Measure();
+        }
+
+        public PrimeNumbersTimeMeasure(Action functionToMeasure, long primeNumbersToCalculate = 4500) : base(functionToMeasure, MeasureReferentialUnit(primeNumbersToCalculate))
+        {
+        }
+    }
+}
---
+++ src/SearchAlgorithms.Core/Testing/Timers/UnifiedUnitTimeMeasure.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace SearchAlgorithms.Core.Testing.Timers
+{
+    public class UnifiedUnitTimeMeasure : TimeMeasure
+    {
+        public class MeasurementResult
+        {
+            public double OriginalResult { get; private set; }
+            public double ResultInReferrentialUnit { get; private set; }
+
+            public MeasurementResult(double originalResult, double resultInReferrentialUnit)
+            {
+                this.OriginalResult = originalResult;
+                this.ResultInReferrentialUnit = resultInReferrentialUnit;
+            }
+        }
+
+        private double ReferrentialUnit;
+
+        public UnifiedUnitTimeMeasure(Action functionToMeasure, double referentialUnit) : base(functionToMeasure)
+        {
+            this.ReferrentialUnit = referentialUnit;
+        }
+
+        public MeasurementResult UnifiedUnitMeasure()
+        {
+            double resultInMs = base.Measure();
+            return new MeasurementResult(resultInMs, resultInMs / ReferrentialUnit);
+        }
+    }
+}
